eae8d757d83e7134a81588a4af5d93cf6e48eae0
Ben Siebert <hello@ben-siebert.de>
2023-12-12 12:05:11 +0100

    finalized

---
+++ src/index.js
@@ -0,0 +1,109 @@
+/**
+ * src/index.js
+ *
+ * Author: Ben Siebert <hello@ben-siebert.de>
+ * Copyright: Copyright (c) 2018-2023 Ben Siebert. All rights reserved.
+ * License: Project License
+ * Created At: 12.12.2023
+ *
+ */
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+const Minio = require("minio");
+const JSZip = require("jszip");
+
+const defaultConfig = {
+  minio: {
+    endpoint: "localhost",
+    port: 9000,
+    accessKey: "minio",
+    securityKey: "minio123",
+    bucket: "backups",
+  },
+  files: ["/home/user"],
+};
+
+const cfgPath = path.join(os.homedir(), ".backup.json");
+
+if (!fs.existsSync(cfgPath)) {
+  fs.writeFileSync(cfgPath, JSON.stringify(defaultConfig, null, 2));
+  console.log("Created default config file");
+  console.log("Please edit the config file at " + cfgPath);
+  process.exit(0);
+}
+
+const cfg = JSON.parse(fs.readFileSync(cfgPath));
+
+if (
+  !cfg.minio.endpoint ||
+  !cfg.minio.accessKey ||
+  !cfg.minio.securityKey ||
+  !cfg.minio.bucket ||
+  !cfg.files
+) {
+  console.log("Please edit the config file at " + cfgPath);
+  process.exit(0);
+}
+
+const minioClient = new Minio.Client({
+  endPoint: cfg.minio.endpoint,
+  port: 443,
+  accessKey: cfg.minio.accessKey,
+  secretKey: cfg.minio.securityKey,
+  useSSL: true,
+});
+
+const backup = async () => {
+  const filePaths = [];
+
+  for (let i = 0; i < cfg.files.length; i++) {
+    console.log("Backup " + cfg.files[i]);
+    const paths = getFilePathsRecursive(cfg.files[i]);
+
+    const zip = new JSZip();
+    for (const path of paths) {
+      const data = fs.readFileSync(path);
+      console.log(path.replace(cfg.files[i], ""));
+      zip.file(path.replace(cfg.files[i], ""), data);
+    }
+    const content = await zip.generateAsync({ type: "nodebuffer" });
+    const tmpFile = path.join(
+      os.tmpdir(),
+      path.basename(cfg.files[i]) + ".zip",
+    );
+    fs.writeFileSync(tmpFile, content);
+    filePaths.push(tmpFile);
+  }
+
+  const rootFolder = new Date().toLocaleString() + "/";
+
+  for (const file of filePaths) {
+    await minioClient.fPutObject(
+      cfg.minio.bucket,
+      rootFolder + path.basename(file),
+      file,
+    );
+  }
+
+  console.log("Backup done");
+};
+
+const getFilePathsRecursive = (dir) => {
+  const files = [];
+
+  fs.readdirSync(dir).forEach((file) => {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+
+    if (stat.isDirectory()) {
+      files.push(...getFilePathsRecursive(filePath));
+    } else {
+      files.push(filePath);
+    }
+  });
+
+  return files;
+};
+
+backup();
